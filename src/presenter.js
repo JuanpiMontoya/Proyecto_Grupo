e213d2f84637d770e4e8497210a60605c6ece1c8
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-10-22 00:19:28 -0400

    Lista de katas detallada por defecto

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const lista_Katas = new ListaDeKatas();
     //Añadimos los katas y su informacion respectiva 
     lista_Katas.añadirkata("Kata - Calculadora String","Cree una calculadora simple que tome una cadena con hasta dos números, separados por comas, y devuelve un número entero de la operacion especifica","Media");
+    lista_Katas.añadirkata("Kata - Punto de venta kata","Cree una aplicación sencilla para escanear códigos de barras para vender productos.","Fácil");
+    lista_Katas.añadirkata("Kata - Kata bancario","Cree una aplicación bancaria sencilla con funciones de depósito, retiro e impresión de estados de cuenta, usando una clase pública, y utilizando cadenas y enteros para fechas y cantidades respectivamente.","Difícil");
     const katas_disp = lista_Katas.devolver_ListaKatas();
 
     //Llenado dinamico del Kata
