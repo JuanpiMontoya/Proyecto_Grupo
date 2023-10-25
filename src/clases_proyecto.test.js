09ab55afbc15d6d28ffc39fb6b3189bcaa42dba1
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-10-24 20:05:16 -0400

    se permite actualizar todos los datos un kata seleccionado

--- src/clases_proyecto.test.js
+++ src/clases_proyecto.test.js
@@ -135,4 +135,14 @@ describe("Editar Katas", () => {
     lista.editarKata(kata_a_editar,"COBO","nueva descripcion","Media")
     expect(kata_a_editar.getDificultad()).toContain("Media"); 
    });
+
+   it("Se permite actualizar la inforación completa de un kata seleccionada", () => { 
+    const lista = new ListaDeKatas();
+    let kata_a_editar =lista.añadirkata("Kata - Kata bancario","Cree una aplicación bancaria sencilla con funciones de depósito, retiro e impresión de estados de cuenta, usando una clase pública, y utilizando cadenas y enteros para fechas y cantidades respectivamente.","Fácil");
+    lista.editarKata(kata_a_editar,"COBO","nueva descripcion","Media")
+    expect(kata_a_editar.getDescripcion()).toContain("nueva descripcion"); 
+    expect(kata_a_editar.getDificultad()).toContain("Media");
+    expect(kata_a_editar.getNombre()).toContain("COBO"); 
+   });
+
 }); 
